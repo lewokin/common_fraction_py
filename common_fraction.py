05caea55594f129b7cba2b411e66d253cdfe9224
Oliwier Grzegorczyk <oliwiergrzegorczyk9@gmail.com>
2026-02-15 23:29:35 +0100

    add Python version check for CommonFraction class

--- common_fraction.py
+++ common_fraction.py
@@ -1,4 +1,9 @@
 from __future__ import annotations
+
+import sys
+if sys.version_info < (3, 10):
+    raise RuntimeError("CommonFraction requires Python 3.10 or newer.")
+
 from functools import total_ordering
 
 #@total_ordering
