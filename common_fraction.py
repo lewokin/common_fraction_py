d4962b7678149ffbf5b1341026dad996d5716bd1
Oliwier Grzegorczyk <oliwiergrzegorczyk9@gmail.com>
2026-02-16 23:06:02 +0100

    implement power method for CommonFraction class

--- common_fraction.py
+++ common_fraction.py
@@ -69,8 +69,11 @@ class CommonFraction:
     def __rtruediv__(self, other: CommonFraction) -> CommonFraction:
         return CommonFraction(other.numerator * self.denominator, other.denominator * self.numerator)
 
-    # def __pow__(self, other: int) -> CommonFraction:
-    #     pass
+    def __pow__(self, other: int) -> CommonFraction:
+        if other < 0:
+            return CommonFraction(self.denominator ** abs(other), self.denominator ** abs(other))
+
+        return CommonFraction(self.numerator ** other, self.denominator ** other)
 
     # def __eq__(self, other: object) -> bool:
     #     pass
