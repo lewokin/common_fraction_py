412fb7f3453cf9d72dc95ac198eed534e3ecb872
Oliwier Grzegorczyk <oliwiergrzegorczyk9@gmail.com>
2026-02-20 22:01:10 +0100

    refactor arithmetic operations in CommonFraction class for conciseness

--- common_fraction.py
+++ common_fraction.py
@@ -31,31 +31,19 @@ class CommonFraction:
         return f"CommonFraction({self.numerator}, {self.denominator})"
     
     def __add__(self, other: CommonFraction) -> CommonFraction:
-        new_self_numerator = self.numerator * other.denominator
-        new_other_numerator = other.numerator * self.denominator
-
-        new_denominator = self.denominator * other.denominator
-
-        return CommonFraction(new_self_numerator + new_other_numerator, new_denominator)
+        return CommonFraction((self.numerator * other.denominator) + (other.numerator * self.denominator), 
+                              self.denominator * other.denominator)
 
     def __radd__(self, other: CommonFraction) -> CommonFraction:
         return self.__add__(other)
 
     def __sub__(self, other: CommonFraction) -> CommonFraction:
-        new_self_numerator = self.numerator * other.denominator
-        new_other_numerator = other.numerator * self.denominator
-
-        new_denominator = self.denominator * other.denominator
-
-        return CommonFraction(new_self_numerator - new_other_numerator, new_denominator)
+        return CommonFraction((self.numerator * other.denominator) - (other.numerator * self.denominator), 
+                              self.denominator * other.denominator)
 
     def __rsub__(self, other: CommonFraction) -> CommonFraction:
-        new_self_numerator = self.numerator * other.denominator
-        new_other_numerator = other.numerator * self.denominator
-
-        new_denominator = self.denominator * other.denominator
-
-        return CommonFraction(new_other_numerator - new_self_numerator, new_denominator)
+        return CommonFraction((other.numerator * self.denominator) - (self.numerator * other.denominator), 
+                              self.denominator * other.denominator)
 
     def __mul__(self, other: CommonFraction) -> CommonFraction:
         return CommonFraction(self.numerator * other.numerator, self.denominator * other.denominator)
@@ -81,11 +69,8 @@ class CommonFraction:
         
         return self.numerator == other.numerator and self.denominator == other.denominator
 
-    def __lt__(self, other: CommonFraction) -> bool:
-        new_self_numerator = self.numerator * other.denominator
-        new_other_numerator = other.numerator * self.denominator
-        
-        return new_self_numerator < new_other_numerator
+    def __lt__(self, other: CommonFraction) -> bool:        
+        return (self.numerator * other.denominator) < (other.numerator * self.denominator)
 
     def __neg__(self) -> CommonFraction:
         return CommonFraction(-self.numerator, self.denominator)
